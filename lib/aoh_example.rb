8baaed73b997c8f897418b87aa2d359cb2edfceb
Max Miller <maxmiller413@gmail.com>
2020-12-04 02:03:55 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -35,8 +35,8 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  aoh[row][key] = "new_value"
-  aoh[row][key]
+  AoH[row][key] = "new_value"
+  AoH[row][key]
   
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
